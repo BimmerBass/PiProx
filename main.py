719dd640e4504bf1f59d34cef61fe09f701fe9ea
Niels Abildskov <49415233+BimmerBass@users.noreply.github.com>
2021-07-14 13:53:00 +0200

    Added the script and a random number generator

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+
+
+def random_num():
+    """
+    random_num generates a random number between 0 and 1.
+
+    :return: A float in [0; 1]
+    """
+    return random.uniform(0.0, 1.0)
